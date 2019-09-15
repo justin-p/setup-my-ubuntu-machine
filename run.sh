2a36a404822128247c4aeee8bbbf03b82492991c
Justin Perdok <justin-p@users.noreply.github.com>
2019-09-15 17:51:29 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+apt update -y && apt upgrade -y && apt install git tmux ncdu gnome-shell-extensions gir1.2-gtop-2.0 gir1.2-networkmanager-1.0 gir1.2-clutter-1.0 zsh git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev libssl1.0-dev -y
