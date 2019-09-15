ac627bd17bd55daefb16684fd18433cd81427a3c
Justin Perdok <justin-p@users.noreply.github.com>
2019-09-15 18:05:08 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-apt update -y && apt upgrade -y && apt install git tmux ncdu gnome-shell-extensions gir1.2-gtop-2.0 gir1.2-networkmanager-1.0 gir1.2-clutter-1.0 zsh git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev libffi-dev libssl1.0-dev -y
+apt update -y && apt upgrade -y && apt install git tmux ncdu gnome-shell-extensions gir1.2-gtop-2.0 gir1.2-networkmanager-1.0 gir1.2-clutter-1.0 zsh  git-core curl zlib1g-dev build-essential libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libffi-dev  -y
+#libssl-dev
+#libcurl4-openssl-dev
+#libssl1.0-dev
