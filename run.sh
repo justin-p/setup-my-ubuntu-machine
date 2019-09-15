918b5107d7f66ced75c2e36c2c04d900e8ff544d
Justin Perdok <justin-p@users.noreply.github.com>
2019-09-15 18:23:25 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-mkdir -p ~/.themes/Flat-Remix-GTK
-git clone https://github.com/daniruiz/Flat-Remix-GTK ~/.themes/Flat-Remix-GTK
+#mkdir -p ~/.themes/Flat-Remix-GTK
+#git clone https://github.com/daniruiz/Flat-Remix-GTK ~/.themes/Flat-Remix-GTK
 apt update -y && apt upgrade -y && apt install git tmux ncdu gnome-tweak-tool gnome-shell-extensions gir1.2-gtop-2.0 gir1.2-networkmanager-1.0 gir1.2-clutter-1.0 zsh  git-core curl zlib1g-dev build-essential libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libffi-dev  -y
 #libssl-dev
 #libcurl4-openssl-dev
